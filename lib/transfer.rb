f696c033e25463728b69578ed176cbd3d4f48c5c
Micah Levine <micah.levine@outlook.com>
2019-09-26 02:30:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,12 +20,10 @@ def execute_transaction
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
-  else
-    if @sender.balance < @amount
+  elsif @sender.balance < @amount
       @status = "rejected"
     return "Transaction rejected. Please check your account balance."
   end
-end
 end
 
 def reverse_transfer
