749d111bf932dbf64741247f8d31001e4bba8119
Micah Levine <micah.levine@outlook.com>
2019-09-26 01:05:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -14,5 +14,17 @@ def deposit(money)
   @balance += money
 end
 
+def display_balance
+  "Your balance is $#{@balance}."
+end
+
+def valid?
+  @status == "open" && @balance > 0 ? true : false
+end
+
+def close_account
+  @status = "closed"
+end
+
 
 end
