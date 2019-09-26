ee4f15449541effe10fcd1a802bc8b62ad547349
Micah Levine <micah.levine@outlook.com>
2019-09-26 02:43:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ def valid?
 end
 
 def execute_transaction
-  binding.pry
+  #binding.pry
   if @sender.balance > @amount && @status == "pending" && valid?
     @sender.balance -= @amount
     @receiver.balance += @amount
